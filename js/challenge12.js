0bed73720c9c40e0ccf6f1a78ea94d5bffa29f5c
Valentin Quiroga <valegolquiroga@gmail.com>
2022-08-31 15:14:06 -0300

    cambios challenge 12

--- js/challenge12.js
+++ js/challenge12.js
@@ -1,20 +1,20 @@
-alert("Hola soy un alerta y te voy a pedir 2 numeros para indicar si el primero de ellos es mayor, menor o igual a 2");
+alert("Hola soy un alerta y te voy a pedir 2 numeros para indicarte si el primero de ellos es mayor, menor o igual al segundo");
 const respuesta1 = prompt("Por favor inserte el primer numero");
 const respuesta2 = prompt("Por favor inserte el segundo numero");
 console.log("Aqui tienes los resultados:")
-respuesta1<2?
-console.log(respuesta1, ' es menor a 2')
+respuesta1<respuesta2?
+console.log(`${respuesta1} es menor a ${respuesta2}`)
 :
-console.log(respuesta1, ' no es menor a 2');
+console.log(`${respuesta1} no es menor a ${respuesta2}`);
 
-respuesta1>2?
-console.log(respuesta1, ' es mayor a 2')
+respuesta1>respuesta2?
+console.log(`${respuesta1} es mayor a ${respuesta2}`)
 :
-console.log(respuesta1, ' no es mayor a 2');
+console.log(`${respuesta1} no es mayor a ${respuesta2}`);
 
-respuesta1==2?
-console.log(respuesta1, ' es igual a 2')
+respuesta1==respuesta2?
+console.log(`${respuesta1} es igual a ${respuesta2}`)
 :
-console.log(respuesta1, ' no es igual a 2');
+console.log(`${respuesta1} no es igual a ${respuesta2}`);
 
 //No se como ponerle pattern de solo numeros al prompt jaja
